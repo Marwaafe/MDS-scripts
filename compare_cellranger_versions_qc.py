7b7d18541adc4794b372f20ce43f141430eb5ae6
Marwaafe <58457914+Marwaafe@users.noreply.github.com>
2025-04-25 11:52:21 +0200

    Update compare_cellranger_versions_qc.py

--- compare_cellranger_versions_qc.py
+++ compare_cellranger_versions_qc.py
@@ -1,87 +1,10 @@
 
 [mafechkar@ares MDS_Data]$ python compare_cellranger_versions_qc.py
-Added row: {'Sample': 'MDS001-09-203', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 16426.0, 'Median_Genes_Per_Cell': 1110.0, 'Fraction_Reads_In_Cells': 96.6, 'Estimated_Cells': 15156.0}
-Added row: {'Sample': 'MDS001-09-203', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 16574.0, 'Median_Genes_Per_Cell': 1118.0, 'Fraction_Reads_In_Cells': 96.3, 'Estimated_Cells': 15020.0}
-Added row: {'Sample': 'MDS005-09-247', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 77711.0, 'Median_Genes_Per_Cell': 1824.0, 'Fraction_Reads_In_Cells': 69.4, 'Estimated_Cells': 3486.0}
-Added row: {'Sample': 'MDS005-09-247', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 80386.0, 'Median_Genes_Per_Cell': 1842.0, 'Fraction_Reads_In_Cells': 68.6, 'Estimated_Cells': 3370.0}
-Added row: {'Sample': 'MDS006-08-249', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 52182.0, 'Median_Genes_Per_Cell': 1418.0, 'Fraction_Reads_In_Cells': 75.5, 'Estimated_Cells': 3649.0}
-Added row: {'Sample': 'MDS006-08-249', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 52848.0, 'Median_Genes_Per_Cell': 1425.0, 'Fraction_Reads_In_Cells': 75.1, 'Estimated_Cells': 3603.0}
-Added row: {'Sample': 'MDS010-09-299', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 17110.0, 'Median_Genes_Per_Cell': 1066.0, 'Fraction_Reads_In_Cells': 74.5, 'Estimated_Cells': 9365.0}
-Added row: {'Sample': 'MDS010-09-299', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 18244.0, 'Median_Genes_Per_Cell': 1110.0, 'Fraction_Reads_In_Cells': 72.9, 'Estimated_Cells': 8783.0}
-Added row: {'Sample': 'MDS016-09-478', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 14453.0, 'Median_Genes_Per_Cell': 1290.0, 'Fraction_Reads_In_Cells': 96.6, 'Estimated_Cells': 14924.0}
-Added row: {'Sample': 'MDS016-09-478', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 14471.0, 'Median_Genes_Per_Cell': 1292.0, 'Fraction_Reads_In_Cells': 96.5, 'Estimated_Cells': 14906.0}
-Added row: {'Sample': 'MDS023-10-053', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 16743.0, 'Median_Genes_Per_Cell': 1279.0, 'Fraction_Reads_In_Cells': 76.7, 'Estimated_Cells': 9938.0}
-Added row: {'Sample': 'MDS023-10-053', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 17791.0, 'Median_Genes_Per_Cell': 1322.0, 'Fraction_Reads_In_Cells': 74.9, 'Estimated_Cells': 9353.0}
-Added row: {'Sample': 'MDS029-10-118', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 224099.0, 'Median_Genes_Per_Cell': 1136.0, 'Fraction_Reads_In_Cells': 65.4, 'Estimated_Cells': 662.0}
-Added row: {'Sample': 'MDS029-10-118', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 232530.0, 'Median_Genes_Per_Cell': 1190.0, 'Fraction_Reads_In_Cells': 64.8, 'Estimated_Cells': 638.0}
-Added row: {'Sample': 'MDS038-10-241', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 26214.0, 'Median_Genes_Per_Cell': 1128.0, 'Fraction_Reads_In_Cells': 84.3, 'Estimated_Cells': 8588.0}
-Added row: {'Sample': 'MDS038-10-241', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 28254.0, 'Median_Genes_Per_Cell': 1143.0, 'Fraction_Reads_In_Cells': 81.8, 'Estimated_Cells': 7968.0}
-Added row: {'Sample': 'MDS059-10-531', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 26170.0, 'Median_Genes_Per_Cell': 776.0, 'Fraction_Reads_In_Cells': 64.2, 'Estimated_Cells': 9715.0}
-Added row: {'Sample': 'MDS059-10-531', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 27548.0, 'Median_Genes_Per_Cell': 796.0, 'Fraction_Reads_In_Cells': 62.9, 'Estimated_Cells': 9229.0}
-Added row: {'Sample': 'MDS065-10-609', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 79216.0, 'Median_Genes_Per_Cell': 1792.0, 'Fraction_Reads_In_Cells': 87.3, 'Estimated_Cells': 2445.0}
-Added row: {'Sample': 'MDS065-10-609', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 79836.0, 'Median_Genes_Per_Cell': 1798.0, 'Fraction_Reads_In_Cells': 87.2, 'Estimated_Cells': 2426.0}
-Added row: {'Sample': 'MDS154-13-486', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 1126584.0, 'Median_Genes_Per_Cell': 718.0, 'Fraction_Reads_In_Cells': 29.7, 'Estimated_Cells': 114.0}
-Added row: {'Sample': 'MDS154-13-486', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 1126584.0, 'Median_Genes_Per_Cell': 718.0, 'Fraction_Reads_In_Cells': 29.7, 'Estimated_Cells': 114.0}
-Added row: {'Sample': 'MDS155-13-606', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 98726.0, 'Median_Genes_Per_Cell': 2000.0, 'Fraction_Reads_In_Cells': 90.9, 'Estimated_Cells': 2413.0}
-Added row: {'Sample': 'MDS155-13-606', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 98931.0, 'Median_Genes_Per_Cell': 2001.0, 'Fraction_Reads_In_Cells': 90.8, 'Estimated_Cells': 2408.0}
-Added row: {'Sample': 'MDS167-13-913', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 26036.0, 'Median_Genes_Per_Cell': 879.0, 'Fraction_Reads_In_Cells': 77.9, 'Estimated_Cells': 8747.0}
-Added row: {'Sample': 'MDS167-13-913', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 27644.0, 'Median_Genes_Per_Cell': 916.0, 'Fraction_Reads_In_Cells': 76.4, 'Estimated_Cells': 8238.0}
-Added row: {'Sample': 'MDS169-13-919', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 38459.0, 'Median_Genes_Per_Cell': 1856.0, 'Fraction_Reads_In_Cells': 93.0, 'Estimated_Cells': 6610.0}
-Added row: {'Sample': 'MDS169-13-919', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 38788.0, 'Median_Genes_Per_Cell': 1867.0, 'Fraction_Reads_In_Cells': 92.9, 'Estimated_Cells': 6554.0}
-Added row: {'Sample': 'MDS180-14-164', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 42678.0, 'Median_Genes_Per_Cell': 822.0, 'Fraction_Reads_In_Cells': 80.5, 'Estimated_Cells': 4390.0}
-Added row: {'Sample': 'MDS180-14-164', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 45741.0, 'Median_Genes_Per_Cell': 841.0, 'Fraction_Reads_In_Cells': 78.5, 'Estimated_Cells': 4096.0}
-Added row: {'Sample': 'MDS189-14-527', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 43549.0, 'Median_Genes_Per_Cell': 1915.0, 'Fraction_Reads_In_Cells': 92.4, 'Estimated_Cells': 7439.0}
-Added row: {'Sample': 'MDS189-14-527', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 43802.0, 'Median_Genes_Per_Cell': 1932.0, 'Fraction_Reads_In_Cells': 92.2, 'Estimated_Cells': 7396.0}
-Added row: {'Sample': 'MDS201-15-093', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 23032.0, 'Median_Genes_Per_Cell': 1275.0, 'Fraction_Reads_In_Cells': 90.6, 'Estimated_Cells': 10131.0}
-Added row: {'Sample': 'MDS201-15-093', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 23815.0, 'Median_Genes_Per_Cell': 1289.0, 'Fraction_Reads_In_Cells': 89.3, 'Estimated_Cells': 9798.0}
-Added row: {'Sample': 'MDS212-15-463', 'Version': '7.2.0', 'Mean_Reads_Per_Cell': 19523.0, 'Median_Genes_Per_Cell': 1208.0, 'Fraction_Reads_In_Cells': 91.6, 'Estimated_Cells': 12506.0}
-Added row: {'Sample': 'MDS212-15-463', 'Version': '9.0.0', 'Mean_Reads_Per_Cell': 20095.0, 'Median_Genes_Per_Cell': 1224.0, 'Fraction_Reads_In_Cells': 90.3, 'Estimated_Cells': 12150.0}
-
- df.head():
-          Sample Version  Mean_Reads_Per_Cell  Median_Genes_Per_Cell  Fraction_Reads_In_Cells  Estimated_Cells
-0  MDS001-09-203   7.2.0              16426.0                 1110.0                     96.6          15156.0
-1  MDS001-09-203   9.0.0              16574.0                 1118.0                     96.3          15020.0
-2  MDS005-09-247   7.2.0              77711.0                 1824.0                     69.4           3486.0
-3  MDS005-09-247   9.0.0              80386.0                 1842.0                     68.6           3370.0
-4  MDS006-08-249   7.2.0              52182.0                 1418.0                     75.5           3649.0
-
- df.columns:
-Index(['Sample', 'Version', 'Mean_Reads_Per_Cell', 'Median_Genes_Per_Cell',
-       'Fraction_Reads_In_Cells', 'Estimated_Cells'],
-      dtype='object')
-
- df_melted.head():
-          Sample   Metric  Value
-0  MDS001-09-203  Version  7.2.0
-1  MDS001-09-203  Version  9.0.0
-2  MDS005-09-247  Version  7.2.0
-3  MDS005-09-247  Version  9.0.0
-4  MDS006-08-249  Version  7.2.0
-
- df_melted.columns:
-Index(['Sample', 'Metric', 'Value'], dtype='object')
-Traceback (most recent call last):
-  File "/net/beegfs/scratch/mafechkar/MDS_Data/compare_cellranger_versions_qc.py", line 96, in <module>
-    g = sns.catplot(
-        ^^^^^^^^^^^^
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/categorical.py", line 2782, in catplot
-    p = Plotter(
-        ^^^^^^^^
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/categorical.py", line 67, in __init__
-    super().__init__(data=data, variables=variables)
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/_base.py", line 634, in __init__
-    self.assign_variables(data, variables)
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/_base.py", line 679, in assign_variables
-    plot_data = PlotData(data, variables)
-                ^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/_core/data.py", line 58, in __init__
-    frame, names, ids = self._assign_variables(data, variables)
-                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/trinity/home/mafechkar/miniconda3/lib/python3.12/site-packages/seaborn/_core/data.py", line 232, in _assign_variables
-    raise ValueError(err)
-ValueError: Could not interpret value `Version` for `hue`. An entry with this name does not appear in `data`.
-
-
+  File "/net/beegfs/scratch/mafechkar/MDS_Data/compare_cellranger_versions_qc.py", line 94
+    t-stat, p_val = ttest_rel(v7.values, v9.values)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+[mafechkar@ares MDS_Data]$
 import os
 import pandas as pd
 import seaborn as sns
