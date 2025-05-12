63e20a4e2e7aca70274b3a721beaf6e341137b7b
Marwaafe <58457914+Marwaafe@users.noreply.github.com>
2025-05-12 15:06:22 +0200

    Update mds-single-cell-qc.r

--- mds-single-cell-qc.r
+++ mds-single-cell-qc.r
@@ -74,12 +74,8 @@ print(seurat_obj)
 seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern = "^MT-")
 
 # Step 6: (Optional but recommended) Remove cells with missing QC values before plotting
-seurat_obj <- subset(
-  seurat_obj,
-  subset = !is.na(seurat_obj$nFeature_RNA) &
-           !is.na(seurat_obj$nCount_RNA) &
-           !is.na(seurat_obj$percent.mt)
-)
+seurat_obj <- subset(seurat_obj, subset = nFeature_RNA > 0 & nCount_RNA > 0 & percent.mt >= 0)
+
 
 # Step 7: Normalize RNA
 seurat_obj <- NormalizeData(seurat_obj, normalization.method = "LogNormalize", assay = "RNA")
